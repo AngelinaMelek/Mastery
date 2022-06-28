4ae916117f6a14613d883d363dbc4b4da47eceab
Anhelina_Lebedieva <angelina.lebedieva@gmail.com>
2022-06-28 11:13:19 +0300

    button for modal window

--- Landing/script.js
+++ Landing/script.js
@@ -1,34 +1,41 @@
 function navLinkActive(event) {
-  const a = document.querySelector(".nav-link--active");
-  event.stopPropagation();
-  event.target.classList.add("nav-link--active");
-  a.classList.remove("nav-link--active");
   console.log(event);
+  const a = document.querySelector(".nav-link--active");
+  a.classList.remove("nav-link--active");
+  console.log(event.target.tagName);
+  if (event.target.tagName === "A") {
+    event.target.classList.add("nav-link--active");
+  } else {
+    const innerLink = event.target.querySelector("a.nav-link");
+    console.log(innerLink);
+    innerLink.classList.add("nav-link--active");
+  }
 }
 
 const links = document.querySelector(".nav-box");
 
 links.addEventListener("click", navLinkActive, true);
 
-let btns = document.querySelectorAll("*[data-modal-btn]");
+const btns = document.querySelectorAll("*[data-modal-btn]");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    let name = btns[i].getAttribute("data-modal-btn");
-    let modal = document.querySelector("[data-modal-window='" + name + "']");
+    const name = btns[i].getAttribute("data-modal-btn");
+    const modal = document.querySelector("[data-modal-window='" + name + "']");
     modal.style.display = "block";
   });
-  let modal = document.querySelector("[data-modal-window]");
-  let close = modal.querySelector(".close-modal-window");
+  const modal = document.querySelector("[data-modal-window]");
+  const close = modal.querySelector(".close-modal-window");
   close.addEventListener("click", function () {
     modal.style.display = "none";
   });
 }
 
-window.onclick = function (event) {
+window.addEventListener("click", function (event) {
   if (event.target.hasAttribute("data-modal-window")) {
-    let modals = document.querySelectorAll("*[data-modal-window]");
+    const modals = document.querySelectorAll("*[data-modal-window]");
     for (let i = 0; i < modals.length; i++) {
       modals[i].style.display = "none";
     }
   }
-};
+  console.log("close-modal-window");
+});
