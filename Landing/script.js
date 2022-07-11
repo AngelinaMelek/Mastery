89a2801482232b709ac494f28f91bb5b0ffeeae3
Anhelina_Lebedieva <angelina.lebedieva@gmail.com>
2022-07-11 14:40:41 +0300

    Upgrade of window close option

--- Landing/script.js
+++ Landing/script.js
@@ -17,25 +17,21 @@ const links = document.querySelector(".nav-box");
 links.addEventListener("click", navLinkActive, true);
 
 const btns = document.querySelectorAll("*[data-modal-btn]");
+const modal = document.querySelector("[data-modal-window]");
+const close = modal.querySelector(".close-modal-window");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     const name = btns[i].getAttribute("data-modal-btn");
     const modal = document.querySelector("[data-modal-window='" + name + "']");
     modal.style.display = "block";
   });
-  const modal = document.querySelector("[data-modal-window]");
-  const close = modal.querySelector(".close-modal-window");
-  close.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
 }
+close.addEventListener("click", function () {
+  modal.style.display = "none";
+});
 
 window.addEventListener("click", function (event) {
   if (event.target.hasAttribute("data-modal-window")) {
-    const modals = document.querySelectorAll("*[data-modal-window]");
-    for (let i = 0; i < modals.length; i++) {
-      modals[i].style.display = "none";
-    }
+    modal.style.display = "none";
   }
-  console.log("close-modal-window");
 });
